aa6ae750082152481b4828757d2481df0b720410
jogampo <jose.gamez.poza@nttdata.com>
2023-06-19 18:47:09 +0200

    Agregamos todos los cambios para que funciona contra una cola

--- lab1/ImportantCompanyApp/Pages/Index.cshtml.cs
+++ lab1/ImportantCompanyApp/Pages/Index.cshtml.cs
@@ -5,12 +5,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
 
 namespace ImportantCompanyApp.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        
+        protected string connectionString = "DefaultEndpointsProtocol=https;AccountName=newqueue9999;AccountKey=zOilU4ChXF/Neb5ZdvoR1pE5TI80/cwovME6/JLAIrDFr6mUCcpYWLoEBX5RQYPVpUcgp8ti0yXV+AStgqxdVA==;EndpointSuffix=core.windows.net";
+        protected string queueName = "ticketrequests";
+
         public string WriteMessage { get; private set; } = "";
 
         public IndexModel(ILogger<IndexModel> logger)
@@ -23,14 +29,27 @@ namespace ImportantCompanyApp.Pages
             GoWriteMessage(message);
         }
 
+       
         public void GoWriteMessage(string message)
         {
+            QueueClient queue = new QueueClient(connectionString, queueName);
+            queue.SendMessage(message);
             WriteMessage += "Message written: " + message + "!\n";
         }
 
         public void OnGetGoReadMessage()
         {
-            WriteMessage += "No messages found";
+            QueueClient queue = new QueueClient(connectionString, queueName);
+            QueueMessage message = queue.ReceiveMessage().Value;
+            if (message is not null)
+            {
+                WriteMessage += message.InsertedOn.ToString() + ": " + message.Body.ToString();
+                queue.DeleteMessage(message.MessageId, message.PopReceipt);
+            }
+            else
+            {
+                WriteMessage += "No messages found.";
+            }
         }
 
     }
